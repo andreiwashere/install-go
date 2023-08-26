9cd3344c800efc976d5b1c0d4e1d85c1e5dd2131
andreiwashere <143206369+andreiwashere@users.noreply.github.com>
2023-08-26 06:49:47 -0700

    Update backup_go.sh

--- backup_go.sh
+++ backup_go.sh
@@ -1,9 +1,45 @@
 #!/bin/bash
 
+######################################################################
+# Script Name: backup_go.sh
+# Description: This script is designed to create backups of the /go directory
+#              and store them in /go/backups. It also deduplicates these backups
+#              to save storage space by creating symlinks to identical backups.
+#
+# Organization:
+# 1. Configuration and Environment Setup
+#    - Setting up script behavior on errors and debug mode.
+#    - Cleanup trap.
+# 2. Functions
+#    - cleanup: Cleans up any lingering lock files.
+#    - safe_exit: Safely exits the script, outputting an error message.
+#    - deduplicate_backup: Checks for duplicate backups, creates symlinks if found.
+#    - create_backup: Creates the backup tarball, calls deduplication.
+#
+# Functions:
+# - cleanup(): Deletes the lock file on script exit or interruption.
+# - safe_exit(message): Exits the script with a custom error message.
+# - deduplicate_backup(new_backup, backup_dir): Checks for duplicates of new_backup
+#     in backup_dir and replaces them with symlinks to save storage.
+#     Also ensures that backups are unique per hostname.
+# - create_backup(backup_dir): Creates a tarball of the /go directory in backup_dir,
+#     excluding certain subdirectories. Calls deduplicate_backup.
+#
+# Developer Notes:
+# - The script must be run as root.
+# - Ensure that the /go directory exists and is the correct target for backup.
+# - The script uses a lock file mechanism to ensure that only one instance runs at a time.
+# - All backups are stored in /go/backups. This directory will be created if it doesn't exist.
+# - The deduplication logic is based on SHA-256 checksums and hostnames.
+# - SECONDS variable is a built-in Bash variable to calculate time duration.
+#
+######################################################################
+
 set -e
 
 [ "${DEBUG}" == "1" ] && set -x
 
+# Usage: cleanup
 cleanup() {
 	[ -f /go/backupgo.lock ] && rm -rf /go/backupgo.lock || echo "Thank you for using backupgo."
 }
@@ -15,17 +51,44 @@ if [ "$(id -u)" -ne 0 ]; then
   exit 1
 fi
 
+# Usage: safe_exit message
 safe_exit() {
 	local msg="${1}"
 	echo "${msg}"
 	exit 1
 }
 
+# Usage: deduplicate_backup new_backup backup_dir
+deduplicate_backup() {
+    local new_backup="$1"
+    local backup_dir="$2"
+    local new_hostname=$(basename "$new_backup" | cut -d '.' -f 2)
+    local new_checksum=$(sha256sum "$new_backup" | awk '{print $1}')
+    local last_matching_backup=""
+    
+    for backup in $(ls -t "$backup_dir"/*.tar.gz); do
+        local existing_hostname=$(basename "$backup" | cut -d '.' -f 2)        
+        [ "$existing_hostname" != "$new_hostname" ] && continue
+        local existing_checksum=$(sha256sum "$backup" | awk '{print $1}')
+        if [ "$existing_checksum" == "$new_checksum" ]; then
+            last_matching_backup="$backup"
+            break
+        fi
+    done
+
+    if [ ! -z "$last_matching_backup" ]; then
+        echo "Found a matching backup: $last_matching_backup"
+        rm "$new_backup"
+        [ ! -L "${new_backup}" ] && ln -s "$last_matching_backup" "$new_backup" || safe_exit "Bailing due to ${new_backup} already existing as a symlink."
+        [ -L "${new_backup}" ] && echo "Created symlink to duplicate backup: ${new_backup}"
+    fi
+}
+
 if [ ! -d /go ]; then
   echo "You must have Go installed at /go via the installgo package."
   exit 1
 fi
-duration=$SECONDS
+
 [ -f /go/backupgo.lock ] && safe_exit "Already running..."
 
 DIR="/go/backups"
@@ -33,11 +96,20 @@ DIR="/go/backups"
 [ ! -d $DIR ] && mkdir -p $DIR
 [ -d $DIR ] && echo "Checked directory: ${DIR}" || safe_exit "Failed to create directory ${DIR}"
 
-echo "$(date)" > /go/backupgo.lock
+# Usage: create_backup backup_dir
+create_backup() {
+    local dir="${1}"
 
-ARCHIVE="go.$(hostname).$(date +%Y.%m.%d).tar.gz"
-echo "Creating backup: ${DIR}/${ARCHIVE}"
-SECONDS=0
-tar -czf "${DIR}/${ARCHIVE}" --exclude="${DIR}" --exclude='/go/root' --exclude='/go/path' --exclude='/go/bin' /go < /dev/null > /dev/null 2>&1
-duration=$SECONDS
-[ -f "${DIR}/${ARCHIVE}" ] && echo "Backup created: ${DIR}/${ARCHIVE} (Completed in ${duration} seconds)" || safe_exit "Failed to capture archive of ${DIR}/${ARCHIVE}"
+    echo "$(date)" > /go/backupgo.lock
+
+    local archive="go.$(hostname).$(date +%Y.%m.%d).tar.gz"
+    echo "Creating backup: ${dir}/${archive}"
+    SECONDS=0
+    tar -czf "${dir}/${archive}" --exclude="${dir}" --exclude='/go/root' --exclude='/go/path' --exclude='/go/bin' /go < /dev/null > /dev/null 2>&1
+    local duration=$SECONDS
+    [ -f "${dir}/${archive}" ] && echo "Backup created: ${dir}/${archive} (Completed in ${duration} seconds)" || safe_exit "Failed to capture archive of ${dir}/${archive}"
+
+    deduplicate_backup "${dir}/${archive}" "${dir}"
+}
+
+create_backup "${DIR}"
