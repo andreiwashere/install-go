268d232e1800258ff89db058bbcb5f72c737de96
andreiwashere <143206369+andreiwashere@users.noreply.github.com>
2023-08-27 19:52:02 -0700

    Update activate_goenv.sh

--- activate_goenv.sh
+++ activate_goenv.sh
@@ -6,12 +6,15 @@ set -u
 set -o pipefail
 set -v
 
+# Initialize prev_dir to something; could also use `pwd` here
+prev_dir=""
+
 function switch_go_version_based_on_file() {
   current_dir=$(pwd)
   
   # Check if the directory has changed
   if [ "$current_dir" != "$prev_dir" ]; then
-    prev_dir="$current_dir"
+    prev_dir="$current_dir"  # Update prev_dir to the new current directory
     
     # Check if we are inside the "/go/projects" directory
     if [[ "$current_dir" == "/go/projects"* ]]; then
@@ -27,3 +30,4 @@ function switch_go_version_based_on_file() {
 
 # Add the function to PROMPT_COMMAND, preserving existing PROMPT_COMMAND
 PROMPT_COMMAND="switch_go_version_based_on_file; $PROMPT_COMMAND"
+
