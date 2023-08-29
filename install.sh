02666732a53a2ee1e153d8ea65794afcd193a503
andreiwashere <143206369+andreiwashere@users.noreply.github.com>
2023-08-29 11:18:28 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 set -e  # BEST PRACTICES: Exit immediately if a command exits with a non-zero status.
-[ "${DEBUG}" == "1" ] && set -x  # DEVELOPER EXPERIENCE: Enable debug mode, printing each command before it's executed.
+[ "${DEBUG:-0}" == "1" ] && set -x  # DEVELOPER EXPERIENCE: Enable debug mode, printing each command before it's executed.
 set -u  # SECURITY: Exit if an unset variable is used to prevent potential security risks.
 set -C  # SECURITY: Prevent existing files from being overwritten using the '>' operator.
+
+safe_exit() {
+  local msg="${1:-UnexpectedError}"
+  echo "${msg}"
+  exit 1
+}
 
 # No Windows support
 [ "$(uname)" != "Linux" ] && safe_exit "HOLD UP: igo is only supported on Linux."
@@ -11,47 +17,51 @@ set -C  # SECURITY: Prevent existing files from being overwritten using the '>' 
 GODIR="${HOME:-"/home/$(whoami)"}/go" # where all things go go
 
 # Create the Go Directory
-[ -d "${GODIR}" ] || mkdir -p "${GODIR}/{backups,manifests,scripts}" || { echo "Failed to mkdir the GODIR ${GODIR}"; exit 1; }
+mkdir -p "${GODIR}/backups" "${GODIR}/manifests" "${GODIR}/scripts"
 
-igo_src="https://raw.githubusercontent.com/andreiwashere/install-go/install_go.sh"
-igo_pkg="${GODIR}/scripts/igo"
-{ [ ! -f "${igo_pkg}" ] && wget --https-only --secure-protocol=auto --no-cache -O "${igo_pkg}" "${igo_src}" < /dev/null > /dev/null 2>&1; } || safe_exit "Downloading ${igo_src}: FAILED"
+download_script() {
+  local src="${1}"
+  local pkg="${2}"
+  local file="${3}"
 
-sgo_src="https://raw.githubusercontent.com/andreiwashere/install-go/switch_go.sh"
-sgo_pkg="${GODIR}/scripts/sgo"
-{ [ ! -f "${sgo_pkg}" ] && wget --https-only --secure-protocol=auto --no-cache -O "${sgo_pkg}" "${sgo_src}" < /dev/null > /dev/null 2>&1; } || safe_exit "Downloading ${sgo_src}: FAILED"
+  if [ ! -f "${pkg}" ] || [ ! -L "${pkg}" ]; then
+    if [ -f "${GODIR}/install-go/${file}" ]; then
+      [ -L "${pkg}" ] || ln -s "${GODIR}/install-go/${file}" "${pkg}"
+    else
+      wget --https-only --secure-protocol=auto --no-cache -O "$pkg" "$src" < /dev/null > /dev/null 2>&1 || echo "Downloading ${src}: FAILED, using local if available."
+    fi
+  else
+    echo "Already have $(basename "${pkg}") installed at ${pkg}!"
+  fi
+}
 
-bgo_src="https://raw.githubusercontent.com/andreiwashere/install-go/backup_go.sh"
-bgo_pkg="${GODIR}/scripts/bgo"
-{ [ ! -f "${bgo_pkg}" ] && wget --https-only --secure-protocol=auto --no-cache -O "${bgo_pkg}" "${bgo_src}" < /dev/null > /dev/null 2>&1; } || safe_exit "Downloading ${bgo_src}: FAILED"
-
-functions_src="https://raw.githubusercontent.com/andreiwashere/install-go/functions.sh"
-functions_file="${GODIR}/scripts/functions.sh"
-{ [ ! -f "${functions_file}" ] && wget --https-only --secure-protocol=auto --no-cache -O "${functions_file}" "${functions_src}" < /dev/null > /dev/null 2>&1; } || safe_exit "Downloading ${functions_src}: FAILED"
+download_script "https://raw.githubusercontent.com/andreiwashere/install-go/install_go.sh" "${GODIR}/scripts/igo" "install_go.sh"
+download_script "https://raw.githubusercontent.com/andreiwashere/install-go/switch_go.sh" "${GODIR}/scripts/sgo" "switch_go.sh"
+download_script "https://raw.githubusercontent.com/andreiwashere/install-go/backup_go.sh" "${GODIR}/scripts/bgo" "backup_go.sh"
+download_script "https://raw.githubusercontent.com/andreiwashere/install-go/functions.sh" "${GODIR}/scripts/functions.sh" "functions.sh"
 
 current_shell=$(basename "${SHELL:-"/bin/bash"}")
 
 case "$current_shell" in
   bash)
-    if [ -f "${HOME}/.bashrc" ]; then
-      ! grep -qxF "export PATH=${GODIR}/scripts" "${HOME}/.bashrc" && echo "export PATH=${GODIR}/scripts:\$PATH" >> "${HOME}/.bashrc"
-      source "${HOME}/.bashrc"
-      echo "Sourced ${HOME}/.bashrc."
-    else
-      echo "${HOME}/.bashrc not found."
-    fi
+    rcfile="${HOME}/.bashrc"
     ;;
   zsh)
-    if [ -f "${HOME}/.zshrc" ]; then
-      ! grep -qxF "export PATH=${GODIR}/scripts" "${HOME}/.zshrc" && echo "export PATH=${GODIR}/scripts:\$PATH" >> "${HOME}/.zshrc"
-      source "${HOME}/.zshrc"
-      echo "Sourced ${HOME}/.zshrc."
-    else
-      echo "${HOME}/.zshrc not found."
-    fi
+    rcfile="${HOME}/.zshrc"
     ;;
   *)
     echo "Unsupported shell: ${current_shell}."
+    exit 1
     ;;
 esac
 
+if [ -f "$rcfile" ]; then
+  ! grep -qxF "export PATH=${GODIR}/scripts" "$rcfile" && echo "export PATH=${GODIR}/scripts:\$PATH" >> "$rcfile"
+  set +u
+  # shellcheck disable=SC1090
+  source "${rcfile}"
+  set -u
+  echo "Sourced $rcfile."
+else
+  echo "$rcfile not found."
+fi
