d6a709d52bd9ef3f82fb7e579a8edfd7c5dd498b
andreiwashere <143206369+andreiwashere@users.noreply.github.com>
2023-08-30 08:09:56 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,10 +35,10 @@ download_script() {
   fi
 }
 
-download_script "https://raw.githubusercontent.com/andreiwashere/install-go/install_go.sh" "${GODIR}/scripts/igo" "install_go.sh"
-download_script "https://raw.githubusercontent.com/andreiwashere/install-go/switch_go.sh" "${GODIR}/scripts/sgo" "switch_go.sh"
-download_script "https://raw.githubusercontent.com/andreiwashere/install-go/backup_go.sh" "${GODIR}/scripts/bgo" "backup_go.sh"
-download_script "https://raw.githubusercontent.com/andreiwashere/install-go/functions.sh" "${GODIR}/scripts/functions.sh" "functions.sh"
+download_script "https://raw.githubusercontent.com/andreiwashere/install-go/main/install_go.sh" "${GODIR}/scripts/igo" "install_go.sh"
+download_script "https://raw.githubusercontent.com/andreiwashere/install-go/main/switch_go.sh" "${GODIR}/scripts/sgo" "switch_go.sh"
+download_script "https://raw.githubusercontent.com/andreiwashere/install-go/main/backup_go.sh" "${GODIR}/scripts/bgo" "backup_go.sh"
+download_script "https://raw.githubusercontent.com/andreiwashere/install-go/main/functions.sh" "${GODIR}/scripts/functions.sh" "functions.sh"
 
 current_shell=$(basename "${SHELL:-"/bin/bash"}")
 
